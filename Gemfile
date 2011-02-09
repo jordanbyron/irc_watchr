99e67fecf88cdb172ac3891442c616962bd569f5
Jordan Byron <jordan@ducksoupsoftware.com>
2011-02-09 08:39:11 -0500

    Add growl notifications

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "http://rubygems.org"
 
 gem "isaac"
 gem "rest-client"
+gem "ruby-growl"
--- irc_watchr.rb
+++ irc_watchr.rb
@@ -4,15 +4,24 @@ require "bundler/setup"
 require 'isaac'
 require_relative "bot_config"
 require 'rest-client'
+require 'ruby-growl'
 
 helpers do
+  def growl
+    @growl ||= Growl.new("127.0.0.1", "irc_watchr", ["message"], nil, GROWL_PASSWORD)
+    
+    @growl
+  end
+  
   def send_message(channel, nick, message)
-    RestClient.post 'https://api.prowlapp.com/publicapi/add', 
+    growl.notify("message", "#{channel}: #{nick}", message)
+    
+    RestClient.post('https://api.prowlapp.com/publicapi/add', 
       :application => channel, 
       :event       => nick, 
       :description => message,
       :apikey      => API_KEY,
-      :priority    => 2
+      :priority    => 2)
   end
 end
 
